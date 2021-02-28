250bba60db1ecc99ba197900affb32becf659dad
Alex Nagy <rebeloper@users.noreply.github.com>
2021-02-28 18:45:44 +0200

    Initial Commit

---
+++ Shared/SwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineApp.swift
+//  Shared
+//
+//  Created by Alex Nagy on 28.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
